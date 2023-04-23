57562379a1ffe95ebec9db4acbdd3c661cbac049
Samdogcom <samvongz@hotmail.com>
2023-04-24 02:27:12 +0800

    优化体积

--- app/src/main/java/com/sam/github/nfc_setting/MainActivity.kt
+++ app/src/main/java/com/sam/github/nfc_setting/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.sam.github.nfc_setting
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
